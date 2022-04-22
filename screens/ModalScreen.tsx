7c9c3bb8d7b5ceaea6a032c70bbebd4831c7ac3a
Fco Salvador Vázquez Roldán <fsvazrol@gmail.com>
2022-04-22 10:44:31 +0200

    Cambios en el FAQ

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { Platform, StyleSheet, Pressable } from "react-native";
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
+import {
+  RootStackParamList,
+  RootTabParamList,
+  RootTabScreenProps,
+} from "../types";
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { MonoText } from "../components/StyledText";
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome } from "@expo/vector-icons";
 import { FAQText } from "../components/FAQList";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const BottomTab = createBottomTabNavigator<RootTabParamList>();
 export default function ModalScreen() {
@@ -14,79 +18,95 @@ export default function ModalScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>FAQ</Text>
       <View style={styles.bloque}>
-        <FAQText>
-          <Text style={styles.subtitle}>
-            ¿preguntaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa?
+        <FAQText style={{ width: 620 }}>
+          <Text style={styles.subtitle1}>
+            ¿Quién conforma el proyecto ORION?
           </Text>
-          <Text style={styles.subtitle}>
-            <br></br>respuesta
+          <Text style={styles.subtitle2}>
+            <br></br>Somos 5 estudiantes de DAM del centro N.S. de los Reyes -
+            Sagrada Familia, Sevilla.
           </Text>
         </FAQText>
+
+        <FAQText style={{ width: 620 }}>
+          <Text style={styles.subtitle1}>
+            ¿Qué nos llevó a desarrollar este proyecto?
+          </Text>
+          <Text style={styles.subtitle2}>
+            <br></br>La pasión por las nuevas tecnologías y las ganas por
+            innovar nos llevaron a embarcarnos en esta aventura.
+          </Text>
+        </FAQText>
+
+        <FAQText style={{ width: 620 }}>
+          <Text style={styles.subtitle1}>
+            ¿En qué se basa el proyecto ORION?
+          </Text>
+          <Text style={styles.subtitle2}>
+            <br></br>Estamos usando la novedosa tecnología de la blockchain,
+            concretamente su implementación en Cardano, ya que es la que mejor
+            futuro tiene.
+          </Text>
+        </FAQText>
+
+        <FAQText style={{ width: 620 }}>
+          <Text style={styles.subtitle1}>
+            He escuchado que el proyecto ORION está concienciado con el medio
+            ambiente ¿Es esto verdad?
+          </Text>
+          <Text style={styles.subtitle2}>
+            <br></br>Sí, los integrantes de ORION tenemos una gran consciencia
+            medioambiental, esta es una de las razones por la que elegimos
+            Cardano, es una de las criptomonedas más respetuiosas con el medio
+            ambiente.
+          </Text>
+        </FAQText>
+
+        <FAQText style={{ width: 620 }}>
+          <Text style={styles.subtitle1}>
+            ¿Cuánto podría esperar ganar con vuestro proyecto?
+          </Text>
+          <Text style={styles.subtitle2}>
+            <br></br>Entre un 4% y un 6% anual, dependiendo de la facturación
+            que tengamos. Aparte de esto hay que sumarle el crecimiento de la
+            propia criptomoneda ¡¡Todo son beneficios!!
+          </Text>
+        </FAQText>
+
+        <FAQText style={{ width: 620 }}>
+          <Text style={styles.subtitle1}>¿Está mi dinero a salvo?</Text>
+          <Text style={styles.subtitle2}>
+            <br></br>Sí, el sistema de la blockchain protege tu dinero contra
+            hackers, para que tu dinero se viera comprometido se deberían de
+            hackear millones de ordenadores a la vez.
+          </Text>
+        </FAQText>
+
         <BottomTab.Screen
-        name="TabOne"
-        component={ModalScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Home',
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Home')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
-        })}
-      />
+          name="TabOne"
+          component={ModalScreen}
+          options={({ navigation }: RootTabScreenProps<"TabOne">) => ({
+            title: "Home",
+            headerRight: () => (
+              <Pressable
+                onPress={() => navigation.navigate("Home")}
+                style={({ pressed }) => ({
+                  opacity: pressed ? 0.5 : 1,
+                })}
+              >
+                <FontAwesome
+                  name="info-circle"
+                  size={25}
+                  style={{ marginRight: 15 }}
+                />
+              </Pressable>
+            ),
+          })}
+        />
       </View>
 
-      <MonoText>Pito</MonoText>
-    
-      <Text style={styles.subtitle2}>
-        ¿Quién conforma el proyecto ORION?
-        <br></br>
-        Somos 5 estudiantes de DAM del centro N.S. de los Reyes - Sagrada
-        Familia, Sevilla.
-      </Text>
-      <Text style={styles.subtitle2}>
-        ¿Qué nos llevó a desarrollar este proyecto?
-        <br></br>
-        La pasión por las nuevas tecnologías y las ganas por innovar nos
-        llevaron a embarcarnos en esta aventura.
-      </Text>
-      <Text style={styles.subtitle2}>
-        ¿En qué se basa el proyecto ORION?
-        <br></br>
-        Estamos usando la novedosa tecnología de la blockchain, concretamente su
-        implementación en Cardano, ya que es la que mejor futuro tiene.
-      </Text>
-      <Text style={styles.subtitle2}>
-        He escuchado que el proyecto ORION está concienciado con el medio
-        ambiente ¿Es esto verdad?
-        <br></br>
-        Sí, los integrantes de ORION tenemos una gran consciencia
-        medioambiental, esta es una de las razones por la que hemos elegido
-        Cardano, es una de las criptomonedas más respetuiosas con el medio
-        ambiente.
-      </Text>
-      <Text style={styles.subtitle2}>
-        ¿Cuánto podría esperar ganar con vuestro proyecto?
-        <br></br>
-        Entre un 4% y un 6% anual, dependiendo de la facturación que tengamos.
-        Aparte de esto hay que sumarle el crecimiento de la propia criptomoneda
-        ¡¡Todo son beneficios!!
-      </Text>
-      <Text style={styles.subtitle2}>
-        ¿Está mi dinero a salvo?
-        <br></br>
-        Sí, el sistema de la blockchain protege tu dinero contra hackers, para
-        que tu dinero se viera comprometido se deberían de hackear millones de
-        ordenadores a la vez.
-      </Text>
+      <MonoText>Probando estilo MonoText</MonoText>
+
       <View
         style={styles.separator}
         lightColor="#eee"
@@ -115,16 +135,13 @@ const styles = StyleSheet.create({
     width: "80%",
   },
   subtitle: {
-  flex: 1,
+    flex: 1,
     fontWeight: "bold",
   },
   subtitle1: {
     textAlign: "justify",
     fontSize: 15,
     fontWeight: "bold",
-    flex: 1,
-    backgroundColor: "#eee",
-    alignItems: "center",
     justifyContent: "center",
     marginRight: "10%",
     marginLeft: "10%",
@@ -150,7 +167,7 @@ const styles = StyleSheet.create({
   },
 });
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
